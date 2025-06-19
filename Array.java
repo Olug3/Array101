eda859c11680af65e05347d72922df5ac3bcb43d
Olug3 <ekorede9@gmail.com>
2025-06-19 12:52:11 +0100

    Update Array101.java

--- Array.java
+++ Array.java
@@ -1,13 +1,46 @@
 import java.util.Scanner;
 
 public class Array{
+    //initialize the array
     static int[] numbers = new int[5];
-    public static void main(String[] args){
+    //Declare the sum variable global
+    static int sum = 0;
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        for(int i=0; i < numbers.length; i++){
-            System.out.println("Enter 5 numbers: ");
+        System.out.println("Enter 5 numbers: ");
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = input.nextInt();
         }
-
+        int sum = Sum(numbers);
+        double average = Average(numbers);
+        int max = maxNumber(numbers);
+        System.out.println("The sum of the 5 numbers are " + sum);
+        System.out.println("The average of the 5 numbers are " + average);
+        System.out.println("The maximum number is " + max);
     }
+    public static int Sum(int[] arr){
+
+        for(int i =0; i<numbers.length; i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    public static double Average(int[] arr){
+        double avg;
+        avg = (double) sum / numbers.length;
+        return avg;
+    }
+
+    public static int maxNumber(int[] arr){
+        int max = numbers[0];
+        for (int i =0; i < numbers.length; i++){
+            if(numbers[i] > max){
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+
+
 }
